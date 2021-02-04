19024166ac50e0b55714d5e286f855d59feae1eb
javier@home <elmasrallao@hotmail.com>
2021-02-04 21:42:45 +0100

    stop right now

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "shoppinglist"
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('menu', views.menu, name="menu"),
-    path('recipe/<int:dish_id>/', views.recipe, name="recipe")
+    path('dish/<int:dish_id>/', views.dish, name="dish"),
+    path('dish/new_dish/', views.new_dish, name="new_dish"),
+    path('dish/save_dish/', views.save_dish, name="save_dish")
 ]
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 import random
 
 from .models import Dish
@@ -13,23 +13,28 @@ def index(request):
     return render(request, "shoppinglist/index.html", context)
 
 
-def recipe(request, dish_id):
+def dish(request, dish_id):
     """ Returns a dish with ingredients and recipe"""
 
     dish = get_object_or_404(Dish, pk=dish_id)
     ingredients = dish.ingredient_set.all()
 
-    context = {
-        'dish': dish,
-        'ingredients': ingredients
-    }
+    context = {"dish": dish, "ingredients": ingredients}
 
-    return render(request, 'shoppinglist/dish.html', context)
+    return render(request, "shoppinglist/dish.html", context)
 
 
 def menu(request):
-
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    """ Get menu for the week """
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
     lunches = [lunch.name for lunch in Dish.objects.filter(time_of_day__lte=2).all()]
     random.shuffle(lunches)
     dinners = [dinner.name for dinner in Dish.objects.filter(time_of_day__gte=2).all()]
@@ -37,9 +42,42 @@ def menu(request):
 
     week = []
     for i in range(6):
-        week.append({'day': days[i], 'lunch': lunches[i], 'dinner': dinners[i]})
-    context = {
-        'week': week
-    }
+        week.append({"day": days[i], "lunch": lunches[i], "dinner": dinners[i]})
+    context = {"week": week}
     print(week)
-    return render(request, 'shoppinglist/weekly_menu.html', context)
+    return render(request, "shoppinglist/weekly_menu.html", context)
+
+
+def new_dish(request):
+    """ Store a new recipe """
+    context = {
+        "types": ["carbs", "meat", "fish", "veg"],
+        "time_of_day": ["lunch", "diner", "both"]
+    }
+    return render(request, "shoppinglist/new_dish.html", context)
+
+
+def save_dish(request):
+    data = request.POST
+    time_of_day_mapping = {
+        "lunch": 1,
+        "both": 2,
+        "diner": 3
+    }
+    dish = Dish(
+        name=data['dish_name'],
+        kind=data['type'],
+        recipe=data['recipe'],
+        link=data['link_to_recipe'],
+        time_of_day=time_of_day_mapping[data['moment']],
+    )
+    dish.save()
+    return HttpResponse("Dish {} saved".format(data['dish_name']))
+
+
+def test(request):
+    dish = get_object_or_404(Dish, pk=76)
+    ingredients = dish.ingredient_set.all()
+
+    context = {"dish": dish, "ingredients": ingredients}
+    return render(request, "shoppinglist/dish.html", context)
